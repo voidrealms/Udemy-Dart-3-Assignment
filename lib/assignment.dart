492ac264acc204a589e114ab9272a5e4d3089a6e
Richard Macwoods <59056174+Ultra-rrich@users.noreply.github.com>
2023-03-16 18:51:51 -0700

    Update assignment.dart

--- lib/assignment.dart
+++ lib/assignment.dart
@@ -1,3 +1,6 @@
-int calculate() {
-  return 6 * 7;
-}
+main(List<String> arguments) {
+
+  List<int> numbers = [1,5,3,25,12,9,22,4,52,552,86];
+  
+  print(numbers.index)
+   print(numbers.remove(8))
